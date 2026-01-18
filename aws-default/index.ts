b6ba2f4f95be3db4f5824f613aa5564a2378f28a
vlad-vorobei <vladyslavvorobe@gmail.com>
2026-01-18 15:46:54 +0200

    fix: change core path

--- aws-default/index.ts
+++ aws-default/index.ts
@@ -1,4 +1,4 @@
-import { terruvimDeploy } from 'terruvim';
+import { terruvimDeploy } from 'terruvim-core-test';
 import * as pulumi from '@pulumi/pulumi';
 
 const deployment = terruvimDeploy(__dirname + '/envs');
